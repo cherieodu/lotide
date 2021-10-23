3c56d186a71ae3d516f1fc9b1169f8221cbae4f3
Chérie Oduwole <bellapette0@gmail.com>
2021-10-23 04:40:13 +0000

    changed it from receiving the arrays through the command line to inputting it in the program itself

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -16,6 +16,8 @@ const assertArraysEqual = (array1, array2) => {
   result === true ? console.log("Both arrays are equal.") : console.log("They aren't equal.");
 };
 
-let array1 = process.argv[2];
-let array2 = process.argv[3];
+
+//assign arrays here
+let array1 = [1,2,3,7];
+let array2 = [1,2,3,7];
 assertArraysEqual(array1, array2);
