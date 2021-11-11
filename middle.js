657df3437ae2457d6d58e52424c7caa5d4a82143
Chérie Oduwole <bellapette0@gmail.com>
2021-11-11 03:58:35 +0000

    refactored middle using module.exports

--- middle.js
+++ middle.js
@@ -1,23 +1,3 @@
-//Test functions
-const assertArraysEqual = (array1, array2) => {
-  let result = eqArrays(array1, array2);
-  result === true ? console.log("Both arrays are equal.") : console.log("They aren't equal.");
-};
-
-const eqArrays = (list1, list2) => {
-  if (list1.length === list2.length) {
-    for (let i = 0; i < list1.length; i++) {
-      if (list1[i] !== list2[i]) {
-        return false;
-      }
-    } return true;
-  } else {
-    return false;
-  }
-  
-};
-
-//Middle function
 const middle = (list) => {
   let listLength = list.length;
   let isEven = (listLength % 2 === 0);
@@ -27,5 +7,5 @@ const middle = (list) => {
 
 };
 
-//Test Code
-assertArraysEqual(middle([1, 2, 3]), [1]);
+module.exports = middle;
+
---
+++ test/middleTest.js
@@ -0,0 +1,5 @@
+const assertArraysEqual = require('../assertArraysEqual');
+const middle = require('../middle');
+
+//Test Code
+assertArraysEqual(middle([1, 2, 3]), [1]);
