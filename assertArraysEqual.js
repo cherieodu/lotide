2df666681b2ae91a3abb141321d2a56faf45beec
Chérie Oduwole <bellapette0@gmail.com>
2021-11-11 03:54:08 +0000

    refactored assertArraysEqual using module.exports

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,23 +1,8 @@
-const eqArrays = (list1, list2) => {
-  if (list1.length === list2.length) {
-    for (let i = 0; i < list1.length; i++) {
-      if (list1[i] !== list2[i]) {
-        return false;
-      }
-    } return true;
-  } else {
-    return false;
-  }
-  
-};
+const eqArrays = require('./eqArrays');
 
 const assertArraysEqual = (array1, array2) => {
   let result = eqArrays(array1, array2);
   result === true ? console.log("Both arrays are equal.") : console.log("They aren't equal.");
 };
 
-
-//assign arrays here
-let array1 = [1,2,3,7];
-let array2 = [1,2,3,7];
-assertArraysEqual(array1, array2);
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,6 @@
+const assertArraysEqual = require('../assertArraysEqual');
+
+//assign arrays here
+let array1 = [1,2,3,7];
+let array2 = [1,2,3,7];
+assertArraysEqual(array1, array2);
