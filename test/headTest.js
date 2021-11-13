7c7d966fe564dc40c36fe45d9fc355f5d292c0a5
Chérie Oduwole <bellapette0@gmail.com>
2021-11-13 04:36:55 +0000

    removed previous test code

--- test/headTest.js
+++ test/headTest.js
@@ -22,10 +22,3 @@ describe("#head", () => {
   });
 });
 
-// TEST CODE
-// assertEqual(head([5,6,7]), 5);
-// assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// assertEqual(5, head(["now", "is"]));
-// assertEqual(1, head([true, 2, 3]));
-// assertEqual("big", head(["", "bigger", "biggest"]));
-// assertEqual("lower", head([]));
